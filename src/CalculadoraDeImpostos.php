2f70bb236102bfb94d1631c493a0e210339f4930
Davi Lima Nogueira <davi_nogueira@dominio.ufms.br>
2022-06-22 16:45:04 -0400

    Implementação da técnico chamada Strategy

---
+++ src/CalculadoraDeImpostos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+use Alura\DesignPattern\Impostos\Imposto;
+
+class CalculadoraDeImpostos
+{
+    public function calcular(Orcamento $orcamento,Imposto  $imposto): float
+    {
+       return $imposto -> calculaImposto($orcamento);
+       }
+
+}
