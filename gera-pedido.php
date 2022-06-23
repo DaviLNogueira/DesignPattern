e1ee14f7225783f47cba535abb1bf94af0814650
Davi Lima Nogueira <davi_nogueira@dominio.ufms.br>
2022-06-23 09:54:16 -0400

    Inseção de pedidos

---
+++ gera-pedido.php
@@ -0,0 +1,15 @@
+<?php
+
+// todas as strings passadas por linha de comando serão amarzenadas no array argv
+use Alura\DesignPattern\GerarPedido;
+use Alura\DesignPattern\Orcamento;
+use Alura\DesignPattern\Pedido;
+require_once 'vendor/autoload.php';
+
+$valorOrcamento =  $argv[1];
+$numeroDeItens = $argv[2];
+$nomeCliente  = $argv[3];
+
+$gerarPedido = new GerarPedido($valorOrcamento,$numeroDeItens,$nomeCliente);
+$gerarPedido -> execute();
+
---
+++ src/GerarPedido.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+class GerarPedido implements  Command
+{
+    private float $valorOrcamento;
+    private int $numeroItens;
+    private  string $nomeCliente;
+
+    public function __construct(float $valorOrcamento, int $numeroItens, string $nomeCliente)
+    {
+        $this->valorOrcamento = $valorOrcamento;
+        $this->numeroItens = $numeroItens;
+        $this->nomeCliente = $nomeCliente;
+    }
+
+    public function execute()
+    {
+        $orcamento = new Orcamento();
+        $orcamento -> quantidadeItens = $this -> numeroItens;
+        $orcamento -> valor = $this -> valorOrcamento;
+
+        $pedido = new Pedido();
+        $pedido -> dataFinalizacao = new \DateTimeImmutable(); // colocar a hora de hoje;
+        $pedido -> nomeCliente = $this -> nomeCliente;
+        $pedido -> orcamento = $orcamento;
+
+        echo "Cria pedido no banco de dados".PHP_EOL;
+        echo "Envia e-mail para cliente".PHP_EOL;
+
+    }
+}
